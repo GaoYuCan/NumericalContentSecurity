030ca076b2f1fc2c15518e143d19d90161f7de6d
Sink <468766131@qq.com>
2022-11-05 21:07:57 +0800

    实验二修改为雪花效果

--- ec3/e2/bit_layers.py
+++ ec3/e2/bit_layers.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     bit_layers = [np.zeros_like(lena) for _ in range(8)]
     # 分离出 8 个位平面
     for i in range(8):
-        bit_layers[i] = lena & (1 << i)
+        bit_layers[i] = ((lena >> i) & 1) << 7
         cv.imshow(f'L{i + 1}', bit_layers[i])
     # 修改 1 - 3，嵌入随机水印
     W, H = lena.shape
@@ -16,6 +16,6 @@ if __name__ == '__main__':
         for y in range(H):
             lena_water[x, y] &= 0b11111000
             lena_water[x, y] |= random.randint(0b000, 0b111)
-    cv.imshow(f'S', lena) # 原图
-    cv.imshow(f'W',lena_water) # 嵌入随机水印的图
+    cv.imshow(f'S', lena)  # 原图
+    cv.imshow(f'W', lena_water)  # 嵌入随机水印的图
     cv.waitKey()
